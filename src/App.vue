e49977ff1a39c235bfa4e733309afa6142681b80
Towfik Hasan <tufikhasan05@gmail.com>
2023-11-20 01:00:08 +0600

    v-show directive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 const result = true;
-const day = "friday";
+const day = "sunday";
 </script>
 <template>
-  <h1 v-if="result">Result</h1>
-  <h1 v-if="result == 1">Result</h1>
+  <h1 v-show="result">Result</h1>
+  <h1 v-show="result == 1">Result</h1>
 
-  <h2 v-if="day == 'sunday'">Sunday</h2>
-  <h2 v-else-if="day == 'monday'">Monday</h2>
-  <h2 v-else-if="day == 'tuesday'">Tuesday</h2>
-  <h2 v-else>Today Is a {{ day }}</h2>
+  <h2 v-show="day == 'sunday'">Sunday</h2>
+  <h2 v-show="day == 'monday'">Monday</h2>
+  <h2 v-show="day == 'tuesday'">Tuesday</h2>
 </template>
 
 <style scoped></style>
