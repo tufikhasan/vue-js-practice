ecd3dde515ce7bb76163eb86adc8a8781ff81eb5
Towfik Hasan <tufikhasan05@gmail.com>
2023-11-19 22:35:54 +0600

    even odd checker with negative value

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ const number = ref();
 </script>
 <template>
   <h1 v-show="number">
-    {{ number }} is {{ number % 2 == 0 ? "Even" : "Odd" }} number
+    {{ number }} is {{ number < 0 ? "negative" : "" }}
+    {{ number % 2 == 0 ? "Even" : "Odd" }} number
   </h1>
   <input
     type="number"
