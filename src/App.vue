cd0160baffcc21bd99e1dd1762f3f5c5afd2fcf4
Towfik Hasan <tufikhasan05@gmail.com>
2023-11-19 22:00:05 +0600

    ternary expression

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,28 @@
 <script setup>
-// object
-const article = {
-  title: "Crypto Exchanges, Not Just FTX, Are All a Mess Right Now",
-  description:
-    "The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes on Binance declined, according to a Bloomberg report Friday.…",
-  url: "https://gizmodo.com/crypto-exchanges-ftx-binance-genesis-gemini-are-a-mess-1850968083",
-  urlToImage:
-    "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/46326bbf3c33c4ddb3f18069c82167d7.jpg",
-  content:
-    "The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes … [+1852 chars]",
-  buttonText: "See more",
-};
-// array
-const news = [
-  {
-    title: "Crypto Exchanges, Not Just FTX, Are All a Mess Right Now",
-    description:
-      "The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes on Binance declined, according to a Bloomberg report Friday.…",
-    url: "https://gizmodo.com/crypto-exchanges-ftx-binance-genesis-gemini-are-a-mess-1850968083",
-    urlToImage:
-      "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/46326bbf3c33c4ddb3f18069c82167d7.jpg",
-    content:
-      "The founders of cryptocurrency exchanges face a mountain of regulatory challenges and billions in personal losses. Binance CEO Changpeng Zhao personally lost $12 billion this year as trading volumes … [+1852 chars]",
-    buttonText: "See more",
-  },
-];
+const day = "fri";
+const weather = "s";
 </script>
 <template>
-  <section>
-    <!-- Display object data -->
-    <div>
-      <img v-bind:src="article.urlToImage" v-bind:alt="article.title" />
-      <h1>{{ article.title }}</h1>
-      <p>{{ article.content }}</p>
-      <a v-bind:href="article.url" target="_blank">{{ article.buttonText }}</a>
-    </div>
-    <!-- Display array data -->
-    <div>
-      <img v-bind:src="news[0].urlToImage" v-bind:alt="news[0].title" />
-      <h1>{{ news[0].title }}</h1>
-      <p>{{ news[0].content }}</p>
-      <a v-bind:href="news[0].url" target="_blank">{{ news[0].buttonText }}</a>
-    </div>
-  </section>
+  <!-- ternary operator -->
+  <!-- <h1>{{ day == "sun" ? "Weekend" : "No weekend" }}</h1>
+  <br /> -->
+
+  <!--multiple condition -  ternary operator -->
+  <!-- <h1>
+    {{ day == "sun" ? "Weekend" : "No weekend" }}
+    {{ weather == "s" ? "Today is a sunny day." : "Winter" }}
+  </h1>
+  <br /> -->
+
+  <!--multiple condition -  ternary operator -->
+  <h1>
+    {{ day == "sun" ? "Weekend" : day == "fri" ? "Zummah" : "work day" }}
+  </h1>
+  <br />
 </template>
 
 <style scoped>
-section {
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
-}
-div {
-  max-width: 300px;
-}
-img {
-  width: 100%;
-}
 h1 {
-  font-size: 2rem;
+  color: red;
 }
 </style>
