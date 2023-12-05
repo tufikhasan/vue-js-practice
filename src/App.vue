3f8866d951e0877cd01069dc75fe97600de1173d
Towfik Hasan <tufikhasan05@gmail.com>
2023-12-06 00:56:02 +0600

    multiple ternary condition

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
-<script setup></script>
+<script setup>
+const data = {
+  name: "Towfik",
+  age: 25,
+};
+</script>
 
 <template>
-  <h2>When working event always use v-show condition not v-if</h2>
-  <h2>v-if can use template tag but v-show not working template tag</h2>
-  <template v-if="true">
-    <h1>v-if: working template tag</h1>
-  </template>
-  <div v-if="true">
-    <h1>v-if: show all tag with parent tag</h1>
-  </div>
-
-  <br />
-
-  <template v-show="true">
-    <h1>v-show: not working template tag</h1>
-  </template>
-  <div v-show="true">
-    <h1>v-show: always show all tag with parent tag</h1>
-  </div>
+  <h1
+    :class="[
+      data.name === 'Towfik' ? 'btn-primary' : 'btn-secondary',
+      data.age === 25 ? 'adult' : 'child',
+    ]"
+  >
+    Hello
+  </h1>
 </template>
 
-<style scoped>
-h2 {
-  color: red;
-}
-</style>
+<style scoped></style>
